da3b746208ca79c0ea65aecad53dddc07a3c5da0
gbirla11-cmd <gbirla11@gmail.com>
2026-02-24 13:05:39 +0530

    Create LandingPage component

---
+++ src/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Landing Page</h1>
+            <p>This is the home of our amazing application.</p>
+        </div>
+    );
+};
+
+export default LandingPage;
